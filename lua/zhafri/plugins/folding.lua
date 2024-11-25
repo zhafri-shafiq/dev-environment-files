dc7156f19c851187a60ab7ab1689461659ec33a1
Zhafri Shafiq <zhafri.noor@silentmode.my>
2024-11-26 01:46:37 +0800

    feat: modify folding

--- lua/zhafri/plugins/folding.lua
+++ lua/zhafri/plugins/folding.lua
@@ -60,54 +60,54 @@ return {
 			vim.opt.foldlevelstart = 99
 		end,
 		opts = {
-			-- 	-- when opening the buffer, close these fold kinds
-			-- 	close_fold_kinds_for_ft = {
-			-- 		default = { "imports", "comment" },
-			-- 		json = { "array" },
-			-- 		-- use `:UfoInspect` to get see available fold kinds
-			-- 	},
-			-- 	open_fold_hl_timeout = 800,
-			-- 	provider_selector = function(_, ft, buftype)
-			-- 		-- PERF disable folds on `log`, and only use `indent` for `bib` files
-			-- 		if ft == "log" then
-			-- 			return ""
-			-- 		end
-			-- 		-- ufo accepts only two kinds as priority, see https://github.com/kevinhwang91/nvim-ufo/issues/256
-			-- 		local useIndent = { "csv", "applescript", "conf", "" }
-			-- 		if buftype ~= "" or vim.startswith(ft, "git") or vim.tbl_contains(useIndent, ft) then
-			-- 			return "indent"
-			-- 		end
-			-- 		return { "lsp", "treesitter" }
-			-- 	end,
-			-- 	-- show folds with number of folded lines instead of just the icon
-			-- 	fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-			-- 		local hlgroup = "NonText"
-			-- 		local icon = ""
-			-- 		local newVirtText = {}
-			-- 		local suffix = ("  %s %d"):format(icon, endLnum - lnum)
-			-- 		local sufWidth = vim.fn.strdisplaywidth(suffix)
-			-- 		local targetWidth = width - sufWidth
-			-- 		local curWidth = 0
-			-- 		for _, chunk in ipairs(virtText) do
-			-- 			local chunkText = chunk[1]
-			-- 			local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-			-- 			if targetWidth > curWidth + chunkWidth then
-			-- 				table.insert(newVirtText, chunk)
-			-- 			else
-			-- 				chunkText = truncate(chunkText, targetWidth - curWidth)
-			-- 				local hlGroup = chunk[2]
-			-- 				table.insert(newVirtText, { chunkText, hlGroup })
-			-- 				chunkWidth = vim.fn.strdisplaywidth(chunkText)
-			-- 				if curWidth + chunkWidth < targetWidth then
-			-- 					suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-			-- 				end
-			-- 				break
-			-- 			end
-			-- 			curWidth = curWidth + chunkWidth
-			-- 		end
-			-- 		table.insert(newVirtText, { suffix, hlgroup })
-			-- 		return newVirtText
-			-- 	end,
+			-- when opening the buffer, close these fold kinds
+			close_fold_kinds_for_ft = {
+				default = { "imports", "comment" },
+				json = { "array" },
+				-- use `:UfoInspect` to get see available fold kinds
+			},
+			open_fold_hl_timeout = 800,
+			provider_selector = function(_, ft, buftype)
+				-- PERF disable folds on `log`, and only use `indent` for `bib` files
+				if ft == "log" then
+					return ""
+				end
+				-- ufo accepts only two kinds as priority, see https://github.com/kevinhwang91/nvim-ufo/issues/256
+				local useIndent = { "csv", "applescript", "conf", "" }
+				if buftype ~= "" or vim.startswith(ft, "git") or vim.tbl_contains(useIndent, ft) then
+					return "indent"
+				end
+				return { "lsp", "treesitter" }
+			end,
+			-- show folds with number of folded lines instead of just the icon
+			fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+				local hlgroup = "NonText"
+				local icon = ""
+				local newVirtText = {}
+				local suffix = ("  %s %d"):format(icon, endLnum - lnum)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				table.insert(newVirtText, { suffix, hlgroup })
+				return newVirtText
+			end,
 		},
 	},
 }
